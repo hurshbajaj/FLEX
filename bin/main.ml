11b6d26ad8032602a9cf39d5c73f03a07abdc177
Hursh Bajaj <hurshbajaj2011@gmail.com>
2025-12-12 18:24:53 +0530

    undo + refactore

--- bin/main.ml
+++ bin/main.ml
@@ -4,30 +4,12 @@ open Unix
 
 exception Break
 
+(* TYPES *)
 type logger = {
     oc: out_channel;
     file: string;
     lock: Mutex.t;
 }
-let get_logger file = 
-    let oc = open_out file in
-    {
-        oc;
-        file;
-        lock = Mutex.create ()
-    }
-let log loggr content = 
-    Mutex.lock loggr.lock;
-    output_string loggr.oc (content^"\n");
-    flush loggr.oc;
-    Mutex.unlock loggr.lock
-
-let logger_done loggr = 
-    Mutex.lock loggr.lock;
-    let _ = open_out loggr.file in
-    close_out loggr.oc
-
-let loggr = get_logger "flex.log"
 
 type mode = Mode_Edt | Mode_Jmp
 
@@ -45,35 +27,23 @@ type action =
     | Act_VpShiftX  
 
     | Act_Pending of char
-    | Act_KillLine
-let last_act = ref Act_NONE
+    | Act_Undo
+    | Act_KillLine | Act_InsertLine of int * string
 
 type buffer = {
     file: string;
     mutable lines: string list;
 }
+
 type viewport = {
     mutable top: int;
     mutable left: int;
 }
-let viewport_of_ctx buffer size = 
-    {
-        top = 0;
-        left = 0;
-    }
 
-let buffer_of_file file = 
-    let ic = open_in file in
-    let len = in_channel_length ic in
-    let content = really_input_string ic len in
-    close_in ic;
-    {
-        file = file;
-        lines = String.split_on_char '\n' content;
-    }
 type act_info = {
     mutable vp_shift: int;
 }
+
 type status = {
     mutable status_i: int;
     mutable status_len: int;
@@ -85,6 +55,7 @@ type status = {
     mutable gap_: int;
     mutable toggled: bool;
 }
+
 type editor = {
     buffer: buffer;
     viewport: viewport;
@@ -99,9 +70,91 @@ type editor = {
     status: status;
 
     act_info: act_info;
+
+    mutable undo_lst: action list;
 }
 
+(* HELPER *)
+
+let is_some = function
+  | Some _ -> true
+  | None -> false
+
+let string_of_mode mode = match mode with
+| Mode_Jmp -> "JMP"
+| Mode_Edt -> "EDT"
+
+(* TYPE METHODS *)
+
+let get_logger file = 
+    let oc = open_out file in
+    {
+        oc;
+        file;
+        lock = Mutex.create ()
+    }
+let log loggr content = 
+    Mutex.lock loggr.lock;
+    output_string loggr.oc (content^"\n");
+    flush loggr.oc;
+    Mutex.unlock loggr.lock
+let logger_done loggr = 
+    Mutex.lock loggr.lock;
+    let _ = open_out loggr.file in
+    close_out loggr.oc
+let loggr = get_logger "flex.log"
+
+let last_act = ref Act_NONE
+
+let viewport_of_ctx buffer size = 
+    {
+        top = 0;
+        left = 0;
+    }
+
+(* -> BUF *)
+
+let buffer_of_file file = 
+    let ic = open_in file in
+    let len = in_channel_length ic in
+    let content = really_input_string ic len in
+    close_in ic;
+    {
+        file = file;
+        lines = String.split_on_char '\n' content;
+    }
+let insert_str s idx ch =
+    let len = String.length s in
+    match ch with
+    | Some ch -> String.sub s 0 idx ^ String.make 1 ch ^ String.sub s idx (len - idx)
+    | None -> String.sub s 0 (idx-1) ^ String.sub s idx (len - idx)
+let lst_replace_at i x lst =
+    let rec aux idx = function
+    | [] -> []
+    | _ :: tl when idx = i -> x :: tl
+    | hd :: tl -> hd :: aux (idx + 1) tl
+    in
+    aux 0 lst
+let lst_remove_at idx lst =
+    let rec aux i = function
+    | [] -> []
+    | _ :: tl when i = idx -> tl
+    | hd :: tl -> hd :: aux (i + 1) tl
+    in aux 0 lst
+let lst_insert_at idx str lst = 
+    let rec aux i = function
+        | [] -> if idx = i then str::[] else []
+        | hd :: tl when i = idx -> str::hd::tl
+        | hd::tl -> hd :: aux (i + 1) tl
+    in aux 0 lst
+
 let cursor_to cy cx = Printf.printf "\027[%d;%dH%!" cy cx
+let update_cursor_style edtr = 
+    match edtr.mode with
+    | Mode_Edt -> print_string "\027[6 q"
+    | Mode_Jmp -> if not (is_some edtr.pending ) then print_string "\027[2 q" else print_string "\027[3 q"
+
+(* PRELIMS *)
 
 let raw_mode  fd = 
     let attr = tcgetattr fd in
@@ -115,12 +168,6 @@ let raw_mode  fd =
 
 let disable_raw fd old = tcsetattr fd TCSANOW old
 
-let print_char_a b =
-    if b >= 32 && b <= 126 then
-        print_char (char_of_int b)
-    else
-        () 
-
 let alt_screen cmd = 
     if cmd = 1 then (print_string "\027[?1049h") else print_string "\027[?1049l";
     flush Stdlib.stdout
@@ -133,14 +180,22 @@ let clear () =
     print_string "\027[2J\027[H";
     flush Stdlib.stdout
 
+(* CHARS & BYTES *)
+
+let print_char_a b =
+    if b >= 32 && b <= 126 then
+        print_char (char_of_int b)
+    else
+        () 
+
 let safe_read fd buf pos len =
-  let rec aux () =
-    try
-      read fd buf pos len
+    let rec aux () =
+        try
+            read fd buf pos len
     with
     | Unix.Unix_error (Unix.EINTR, _, _) -> aux ()
-  in
-  aux ()
+    in
+    aux ()
 
 let read_bytes () = 
     let buf = Bytes.create 5 in
@@ -151,55 +206,45 @@ let read_char () =
     let b1, b2, _, _, _ = read_bytes () in
     if b1 = '\027' && ( b2 = '[' || b2 = 'O' ) then ( '\000', '\000' ) else (if b1 = '\027' then ( let bn, _, _, _, _ = read_bytes () in ( b1, bn ) ) else (b1, '\000') )
 
-let string_of_mode mode = match mode with
-| Mode_Jmp -> "JMP"
-| Mode_Edt -> "EDT"
-
-let insert_buf s idx ch =
-  let len = String.length s in
-  match ch with
-  | Some ch -> String.sub s 0 idx ^ String.make 1 ch ^ String.sub s idx (len - idx)
-  | None -> String.sub s 0 (idx-1) ^ String.sub s idx (len - idx)
-
 let rgb r g b text = Printf.sprintf "\027[38;2;%d;%d;%dm%s\027[0m" r g b text
-let is_some = function
-  | Some _ -> true
-  | None -> false
+
+(* DRAW *)
 
 let draw_status edtr =
-  let highlight = rgb 212 118 215 in
-  let has_pending = is_some edtr.pending in
-  let mode_str = string_of_mode edtr.mode in
-  
-  let content, visual_len = if edtr.status.status_i = 0 then
-    let base = mode_str in
-    let visual = (if has_pending then 2 else 0) + String.length base in
-    let display = (if has_pending then (highlight "* ") else "") ^ base in
-    (display, visual)
-  else
-    let text = Printf.sprintf "%d : %d | %s" 
+    let highlight = rgb 212 118 215 in
+    let has_pending = is_some edtr.pending in
+    let mode_str = string_of_mode edtr.mode in
+
+    let content, visual_len = if edtr.status.status_i = 0 then
+        let base = mode_str in
+        let visual = (if has_pending then 2 else 0) + String.length base in
+        let display = (if has_pending then (highlight "* ") else "") ^ base in
+        (display, visual)
+        else
+            let text = Printf.sprintf "%d : %d | %s" 
       (edtr.viewport.top + edtr.cy) 
       (edtr.viewport.left + edtr.cx)
       (Printf.sprintf "%s / %s" 
         (Filename.basename (Filename.dirname (Unix.realpath edtr.buffer.file)))
         edtr.buffer.file) in
-    (text, String.length text)
-  in
-  
-  cursor_to edtr.status.status_row edtr.status.status_start;
-  Printf.printf "%s" (String.make edtr.status.status_len ' ');
-  
-  let new_status_len = visual_len + 5 in
-  let new_status_start = fst(edtr.size) - new_status_len + 1 in
-  let new_status_row = snd edtr.size in
-  
-  cursor_to new_status_row (new_status_start - (if edtr.status.overlap then 2 else 0));
-  Printf.printf "%s" (ANSITerminal.sprintf [ANSITerminal.Bold] "%s" 
+            (text, String.length text)
+            in
+
+    cursor_to edtr.status.status_row edtr.status.status_start;
+    Printf.printf "%s" (String.make edtr.status.status_len ' ');
+
+    let new_status_len = visual_len + 5 in
+    let new_status_start = fst(edtr.size) - new_status_len + 1 in
+    let new_status_row = snd edtr.size in
+
+    cursor_to new_status_row (new_status_start - (if edtr.status.overlap then 2 else 0));
+    Printf.printf "%s" (ANSITerminal.sprintf [ANSITerminal.Bold] "%s" 
     (if edtr.status.overlap then (highlight "| ") else "") ^ content ^ (highlight " <~"));
-  
-  edtr.status.status_len <- new_status_len;
-  edtr.status.status_start <- new_status_start;
-  edtr.status.status_row <- new_status_row
+
+    edtr.status.status_len <- new_status_len;
+    edtr.status.status_start <- new_status_start;
+    
+    edtr.status.status_row <- new_status_row
 
 let draw_viewport edtr = 
     for i=1 to snd edtr.size  do
@@ -238,6 +283,8 @@ let draw edtr =
     cursor_to edtr.cy edtr.cx;
     flush Stdlib.stdout
 
+(* ACTION HANDLING *)
+
 let handle_jmp_ev ev edtr = 
     match ev with
     | 'l', '\000' when (is_some edtr.pending) -> edtr.pending <- None; Act_KillLine
@@ -254,6 +301,7 @@ let handle_jmp_ev ev edtr =
     | 'q', '\000' -> Act_Quit
     | 'i', '\000' -> Act_StatusI
     | 'l', '\000' -> Act_BoL
+    | 'u', '\000' -> Act_Undo
     | '\027', 'l' -> Act_EoL
     | '\027', ' ' -> Act_ModeSwitch (Mode_Edt)
     | '\027', ';' -> Act_VpShiftX
@@ -277,23 +325,7 @@ let handle_ev mode ev edtr =
     | Mode_Jmp -> handle_jmp_ev ev edtr
     | Mode_Edt -> handle_edit_ev ev
 
-let lst_replace_at i x lst =
-  let rec aux idx = function
-    | [] -> []
-    | _ :: tl when idx = i -> x :: tl
-    | hd :: tl -> hd :: aux (idx + 1) tl
-  in
-  aux 0 lst
-
-let lst_remove_at idx lst =
-  let rec aux i = function
-    | [] -> []
-    | _ :: tl when i = idx -> tl
-    | hd :: tl -> hd :: aux (i + 1) tl
-  in aux 0 lst
-
 let rec eval_act action edtr = 
-
     let real_length_ = min (fst edtr.size) ( try max 1 ( String.length ( List.nth edtr.buffer.lines (edtr.viewport.top + edtr.cy - 1) ) ) with Invalid_argument _ -> 1 | Failure nth -> 1 ) in
     let real_length_trim = min (fst edtr.size) ( try max 1 ( String.length ( String.trim ( List.nth edtr.buffer.lines (edtr.viewport.top + edtr.cy - 1) ) ) ) with Invalid_argument _ -> 1 | Failure nth -> 1 ) in
  
@@ -329,7 +361,7 @@ let rec eval_act action edtr =
         | Act_AddChar c -> (
             cursor_to edtr.cy edtr.cx;
             let line = List.nth edtr.buffer.lines (edtr.viewport.top + edtr.cy - 1) in
-            let line_ = insert_buf line (edtr.cx-1) (Some c) in
+            let line_ = insert_str line (edtr.cx-1) (Some c) in
             edtr.buffer.lines <- lst_replace_at (edtr.viewport.top + edtr.cy - 1) line_ edtr.buffer.lines;
             edtr.cx <- edtr.cx + 1
         )
@@ -337,7 +369,7 @@ let rec eval_act action edtr =
             if edtr.cx > 1 then
             cursor_to edtr.cy edtr.cx;
             let line = List.nth edtr.buffer.lines (edtr.viewport.top + edtr.cy - 1) in
-            let line_ = insert_buf line (edtr.cx-1) None in
+            let line_ = insert_str line (edtr.cx-1) None in
             edtr.buffer.lines <- lst_replace_at (edtr.viewport.top + edtr.cy - 1) line_ edtr.buffer.lines;
             edtr.cx <- edtr.cx - 1
         )
@@ -362,7 +394,12 @@ let rec eval_act action edtr =
         | Act_EoL -> edtr.cx <- real_length_
         | Act_BoL -> edtr.cx <- real_length_ - real_length_trim + 1
         | Act_Pending act -> edtr.pending <- Some act
-        | Act_KillLine -> if edtr.cy > 1 then edtr.cy <- edtr.cy - 1; edtr.buffer.lines <- lst_remove_at (edtr.viewport.top + edtr.cy - 1) edtr.buffer.lines 
+        | Act_KillLine -> ( 
+            edtr.undo_lst <- Act_InsertLine ((edtr.viewport.top + edtr.cy - 1), List.nth edtr.buffer.lines (edtr.viewport.top + edtr.cy - 1)) :: edtr.undo_lst;
+            edtr.buffer.lines <- lst_remove_at (edtr.viewport.top + edtr.cy - 1) edtr.buffer.lines; if edtr.cy+edtr.viewport.top - 1 >= List.length edtr.buffer.lines then edtr.cy <- edtr.cy - 1
+        )
+        | Act_Undo -> eval_act (try (List.hd edtr.undo_lst) with _ -> Act_NONE) edtr; edtr.undo_lst <- ( try ( List.tl edtr.undo_lst) with | _ -> [] )
+        | Act_InsertLine (line_no, content) -> log loggr content; edtr.buffer.lines <- lst_insert_at line_no content edtr.buffer.lines
         | _ -> ()
     );
     let real_length_ = min (fst edtr.size) ( try max 1 ( String.length ( List.nth edtr.buffer.lines (edtr.viewport.top + edtr.cy - 1) ) ) with Invalid_argument _ -> 1 | Failure nth -> 1 ) in
@@ -372,10 +409,7 @@ let rec eval_act action edtr =
     if edtr.cy = snd edtr.size && edtr.cx >= edtr.status.status_start - edtr.status.gap then edtr.cx <- max 1 (edtr.status.status_start - edtr.status.gap);
     draw edtr
 
-let update_cursor_style edtr = 
-    match edtr.mode with
-    | Mode_Edt -> print_string "\027[6 q"
-    | Mode_Jmp -> if not (is_some edtr.pending ) then print_string "\027[2 q" else print_string "\027[3 q"
+(* LOOP *)
 
 let run edtr =
     log loggr "\nRunning Editor - - - - - - - - - - - - - - - - - - - - ";
@@ -397,7 +431,6 @@ let run edtr =
     with e -> (if not ( e = Break ) then ( (cleanup fd old); logger_done loggr; raise e));
 
     cleanup fd old; logger_done loggr
-
 let () = 
     let argc = Array.length Sys.argv in
     let file = ref "" in
@@ -433,6 +466,7 @@ let () =
         status;
         act_info;
         pending = None;
+        undo_lst = [];
     } in
 
     Sys.set_signal Sys.sigwinch (Sys.Signal_handle (fun _ -> 
