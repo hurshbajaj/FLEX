cdca0ee3edb72965473b3d5ea3fc5a4cf16c6cda
RustLord547 <hurshbajaj2011@gmail.com>
2026-04-04 21:19:42 +0530

    failed attempt

--- lib/buffer_handles.ml
+++ lib/buffer_handles.ml
@@ -1,113 +1,85 @@
-(*
-    "Mom, can we get ropes?" 
-    "We have ropes at home."
-    Ropes at home:
-*)
+[@@@warning "-a-l-l"]
 
-let max_leaf_weight = 7 (* in bytes *) 
-let max_nodes = 5 (*32*) (*| 8 + ( _2_ * 8 ) |*)
-type metadata = int array (* Node: count; lines; bytes | Piece Table: lines; bytes*) [@@deriving show]
+let m = 3
 
-type buf_type = Buf_add | Buf_org [@@deriving show]
-type piece = {
-    buf_type: buf_type;
-    offset: int;
-    length: int;
-} [@@deriving show]
-type piece_el = Piece_Newline | Piece_None | Piece_Piece of piece [@@deriving show]
-type pieceTable = {
-    metadata: metadata;
-    payload: piece_el list
-} [@@deriving show]
+type piece = int * int * int (* Buffer -> 0 for Org; 1 for Add; 2 for Newline *) (* Offset *) (* Length *)
+type piece_table = PT_VAL of int (* piece list *)
 
-type b_tree = 
-    | Btree_leaf of pieceTable
-    | Btree_internode of {metadata: metadata; payload: (piece_el list) option; children: (b_tree option) array;}
-    [@@deriving show]
+type tree_node_t = Node_inter of { (* keep lhs filled, rhs will be filled only for the last key of the node *)
+    lhs: ((tree_node_t option) array) ref;
+    rhs: ((tree_node_t option) array) ref;
+    ln: int;
+} | Node_leaf of {
+    lhs: tree_node_t option ref;
+    rhs: tree_node_t option ref;
+    ln : int;
+    payload: piece_table;
+}
 
-type buffer = {
-    org_buf: string;
-    add_buf: Buffer.t;
-    tree: b_tree;
-} 
-let hd_piece =
-  function
-  | x :: _ ->x
-  | [] -> Piece_None
+let buf_org = "Hello World!!!"
+let buf_add = Buffer.create 64
 
-let orgBufBreakdown src =
-  let rec aux acc src_ ns =
-    try
-      match src_.[0] with
-      | '\n' ->
-          aux (Piece_Newline :: acc)
-              (String.sub src_ 1 (String.length src_ - 1))
-              (ns + 1)
-      | _ ->
-          match hd_piece acc with
-          | Piece_Newline | Piece_None ->
-              aux
-                (Piece_Piece { buf_type = Buf_org; offset = if hd_piece acc = Piece_Newline then 1 else 0; length = 1 } :: acc)
-                (String.sub src_ 1 (String.length src_ - 1))
-                ns
-          | Piece_Piece temp ->
-              aux
-                (Piece_Piece { buf_type = Buf_org; offset = temp.offset; length = temp.length + 1 } :: List.tl acc)
-                (String.sub src_ 1 (String.length src_ - 1))
-                ns
-    with
-    | Invalid_argument _ -> (List.rev acc, ns)
-  in
-  aux [] src 0
+let tree : tree_node_t option array = Array.make m None
+let root = ref tree
 
-let pieceTableBreakdown pt = (
-    let rec aux byte_count line_count pieceTable_arr md_arr el_i i part_count = ( 
-        try 
-            (
-                let foc_lst = (pieceTable_arr.(el_i)) in
-                let foc_len, nle = match (List.nth foc_lst i) with | Piece_Piece p -> p.length, 0 | Piece_Newline ->  1, 1 | _ -> raise Helper.Unreachable in
-                if byte_count + foc_len > max_leaf_weight && el_i + 1 < Array.length pieceTable_arr then ( (* TODO rebalancing (check second condition) *)
-                    let foc_sect = Helper.sublist i (List.length foc_lst - 1) foc_lst in 
-                    pieceTable_arr.(el_i) <- Helper.sublist 0 (i-1) foc_lst;
-                    aux 0 0 (pieceTable_arr.(el_i + 1) <- foc_sect; pieceTable_arr) md_arr (el_i + 1) 0 (part_count + 1)
-                ) 
-                else (aux (byte_count + foc_len) (line_count+nle) pieceTable_arr (md_arr.(el_i) <- [| line_count + nle; byte_count + foc_len |]; md_arr) el_i (i+1)) part_count
+let get_sep_value = function
+    | Some z -> (
+        match z with
+        | Node_inter n -> n.ln
+        | Node_leaf n -> n.ln
+    )
+    | _ -> max_int
+
+let rec search_op root ln = (
+    let rec bin_search_keys region_covered crnt = ( (* return the index of the element we need to go into *)
+        (Printf.printf "\n[%d, %d], %d" region_covered.(0) region_covered.(1) crnt);
+        if get_sep_value(!root.(crnt)) > ln then (
+            if crnt / 2 <= region_covered.(0) then crnt else (
+                bin_search_keys [|region_covered.(0); crnt|] (crnt/2)
             )
-        with 
-        | _ -> (pieceTable_arr, md_arr, part_count)
-    ) in
-    let temp = Array.make max_nodes [] in
-    temp.(0) <- pt;
-    aux 0 0 temp (Array.make max_nodes [||]) 0 0 1
-)
+        )
+        else if get_sep_value(!root.(crnt)) < ln then (
+            if crnt + (crnt/2) >= region_covered.(1) then (crnt) else (
+                bin_search_keys [|crnt; region_covered.(1)|] (crnt+crnt/2)
+            )
+        ) else crnt
+    ) in 
+    let index_to_follow = bin_search_keys [|-1; m|] (m-1) in 
+    match !root.(index_to_follow) with 
+        | Some z -> ( match z with
+            | Node_inter node -> (
+                if ln < node.ln then search_op node.lhs ln else
+                    if index_to_follow + 1 >= m || !root.(index_to_follow+1) == None then 
+                        search_op node.rhs ln else (
+                            let Some (Node_inter node) = !root.(index_to_follow + 1) in search_op node.lhs ln
+                        )
+                )
+            | Node_leaf node -> node.payload
+        )
+        | _ -> failwith "unreachable"
+)   
 
-let init_buf org = 
-(
-    let pieceTable, orgLen = orgBufBreakdown org in
-    let children, children_md, md_piece_count = pieceTableBreakdown pieceTable in
-    let tree = Btree_internode {
-        metadata = [|md_piece_count; orgLen; String.length org;|]; 
-        payload = None; 
-        children = (
-            let out = Array.make max_nodes None in
-            for i = 0 to Array.length children - 1 do
-                if children.(i) != [] then out.(i) <- Some( Btree_leaf {metadata = children_md.(i); payload = children.(i);})
-            done; out
-        );
-    } in
-    {
-        org_buf = org;
-        add_buf = Buffer.create 4096;
-        tree;
-    }
-)
-
-let exposed_buf_test _ = (
-    let buf = init_buf "HOLA\nHI\nBYE\n" in
-    print_endline ("Add Buffer:" ^ if Buffer.contents buf.add_buf = "" then "" else "\n"); 
-    print_string (Buffer.contents buf.add_buf);
-    print_endline "<-------------------------------------------------------------------->\n";
-    print_endline ("Org Buffer:\n" ^ buf.org_buf);
-    print_endline "<-------------------------------------------------------------------->\n";
-    print_endline (show_b_tree buf.tree)
-)
+let exposed_buf_test _ =
+  (* Build leaves *)
+  let leaf5  = Node_leaf { lhs = ref None; rhs = ref None; ln = 5;  payload = PT_VAL 100 } in
+  let leaf15 = Node_leaf { lhs = ref None; rhs = ref None; ln = 15; payload = PT_VAL 200 } in
+  let leaf25 = Node_leaf { lhs = ref None; rhs = ref None; ln = 25; payload = PT_VAL 300 } in
+  (* Build interior nodes: each Node_inter's lhs points to the leaf
+     that search_op follows when ln < node.ln.
+     rhs is used when ln >= node.ln and there's no next sibling. *)
+  let inter10 = Node_inter {
+    lhs = ref [| Some leaf5;  None; None |];
+    rhs = ref [| None; None; None |];
+    ln  = 10;
+  } in
+  let inter20 = Node_inter {
+    lhs = ref [| Some leaf15; None; None |];
+    rhs = ref [| Some leaf25; None; None |];
+    ln  = 20;
+  } in
+  (* Root array: [inter10 | inter20 | None] *)
+  root := [| Some inter10; Some inter20; None |];
+  (* Test: ln=5  -> goes into inter10.lhs -> leaf5  -> PT_VAL 100 *)
+  let PT_VAL v1 = search_op root 15  in
+  assert (v1 = 200);
+  Printf.printf "All search_op tests passed: %d\n" v1
